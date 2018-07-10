55a8a8ca424d5c6e49bf90f07eb4ce6e7acc9909
amit.kumar <amit.ap1924@gmail.com>
2018-07-10 08:51:10 +0530

    ObservableField

--- app/src/main/java/com/android/databinding/MainActivity.java
+++ app/src/main/java/com/android/databinding/MainActivity.java
@@ -5,19 +5,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.android.databinding.models.UserInfo;
+import com.android.databinding.models.UserInfoObservble;
 
 public class MainActivity extends AppCompatActivity {
 
+    enum Databinding{BASEOBSERVER, OBSERVABLEFIELD}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Databinding databinding = Databinding.OBSERVABLEFIELD;
 
+        switch (databinding){
+            case BASEOBSERVER:{
+                setBaseObserverClass();
+                break;
+            }
+            case OBSERVABLEFIELD:{
+                setObservableFieldClass();
+                break;
+            }
+        }
+    }
+
+    private void setObservableFieldClass() {
+        DataObserverBinding dataObserverBinding = DataBindingUtil.setContentView(this,R.layout.activity_main1);
+        UserInfoObservble userInfoObservble = new UserInfoObservble("Hello","Observer");
+        dataObserverBinding.setUserInfoobserver(userInfoObservble);
+    }
+
+    private void setBaseObserverClass() {
         DataBindingActivity dataBindingActivity = DataBindingUtil.
                 setContentView(this,R.layout.activity_main);
-        UserInfo userInfo = new UserInfo("Hello","world");
+        UserInfo userInfo = new UserInfo("Hello","baseObserver");
 
         dataBindingActivity.setUserInfo(userInfo);
-
     }
 }
---
+++ app/src/main/java/com/android/databinding/models/UserInfoObservble.java
@@ -0,0 +1,30 @@
+package com.android.databinding.models;
+
+
+import android.databinding.Observable;
+import android.databinding.ObservableField;
+import android.databinding.ObservableInt;
+
+public class UserInfoObservble {
+    public ObservableField<String> firstName = new ObservableField<>();
+    public ObservableField<String> lastName = new ObservableField<>();
+    ObservableInt count;
+
+    public UserInfoObservble(String fname, String lname){
+        firstName.set(fname);
+        lastName.set(lname);
+    }
+
+    int toggle = 0;
+    public void onClick(){
+        if(toggle == 0) {
+            firstName.set("First");
+            lastName.set("last");
+            toggle = 1;
+        }else{
+            firstName.set("last");
+            lastName.set("first");
+            toggle = 0;
+        }
+    }
+}
